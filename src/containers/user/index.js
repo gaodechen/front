20a65ead36f746d086ebd90abf4d52dcde99a1d4
Gao Dechen <gaodechen@live.cn>
2019-02-20 21:49:56 +0800

    recommit

--- src/containers/user/index.js
+++
@@ -1,7 +0,0 @@
-import LoginForm from './LoginForm'
-import RegisterForm from './RegisterForm'
-import Logout from './Logout'
-// 登陆态更新HOC
-import withUser from './UserStatus'
-
-export { LoginForm, RegisterForm, Logout, withUser }
